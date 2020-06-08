373959947bdc09bdc9a85d71f799a56ee8e72d7a
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-08 18:47:19 -0300

    Leitura do número de testes

---
+++ src/mediasPonderadasUri1079/Main.java
@@ -0,0 +1,24 @@
+package mediasPonderadasUri1079;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N, i;
+		double media, A, B, C;
+		
+		N = sc.nextInt();
+		
+		
+		
+		sc.close();
+
+
+	}
+
+}
